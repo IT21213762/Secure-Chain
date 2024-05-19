2960ab58ce107c38b8b44a04085e55b5188b4838
“IT21213762” <it21213762@my.sliit.lk>
2024-05-20 00:39:57 +0530

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSBgdClhHGQPV40B4GUaNG5rUHje9kpjI",
-  authDomain: "cloud-storage-solution.firebaseapp.com",
-  databaseURL: "https://cloud-storage-solution-default-rtdb.firebaseio.com",
-  projectId: "cloud-storage-solution",
-  storageBucket: "cloud-storage-solution.appspot.com",
-  messagingSenderId: "679645675510",
-  appId: "1:679645675510:web:49e53558ddda948f12d1d7",
-  measurementId: "G-HW1WL5FV70"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // INITIALISE FIREBASE INSTANCES
